e21c5ddfa227099e5c8cb17c82cb5735b143c28f
Iustin <iustin.esanu7@gmail.com>
2023-04-29 21:17:33 +0300

    delete element

--- script.js
+++ script.js
@@ -1,23 +1,31 @@
 window.onload = function(){
+    
+// Color changing logo
+let logo = document.getElementById("logo");
+logo.addEventListener("click",
+     function(){
+    if(logo.style.color === "firebrick")
+        logo.style.color =  "darkorange";
+    else if(logo.style.color === "darkorange")
+        logo.style.color =  "yellow";
+    else if(logo.style.color === "yellow")
+        logo.style.color = "green";
+    else if(logo.style.color === "green")
+        logo.style.color = "blue";
+    else if(logo.style.color === "blue")
+        logo.style.color = "magenta";
+    else
+        logo.style.color = "firebrick";
+    },   
+    false);
 
-    let logo = document.getElementById("logo");
+    // Delete about-us using Q
+    let about = document.getElementById("about-us");
+    console.log(about);
 
-    logo.onclick = function(){
-        //
-        if(logo.style.color === "firebrick")
-            logo.style.color =  "darkorange";
-        else if(logo.style.color === "darkorange")
-            logo.style.color =  "yellow";
-        else if(logo.style.color === "yellow")
-            logo.style.color = "green";
-        else if(logo.style.color === "green")
-            logo.style.color = "blue";
-        else if(logo.style.color === "blue")
-            logo.style.color = "magenta";
-        else
-            logo.style.color = "firebrick";
-    }
-
-
+    document.addEventListener("keypress", function(event){
+        console.log(event.code);
+        if(event.code === "KeyD")
+            about.remove();
+    })
 }
-
